85d3f5283777f845402347ea54dfc82da2e5aee4
felipetodev <fe.ossandon.u@gmail.com>
2023-01-24 23:14:40 -0300

    add basic eslint config

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+	// ...
+	extends: ['standard', 'plugin:astro/recommended'],
+	rules: {
+		'no-tabs': 'off',
+		indent: 'off',
+		'space-before-function-paren': 'off'
+	},
+	// ...
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro']
+			},
+			rules: {
+				'astro/no-set-html-directive': 'error'
+			}
+		}
+	]
+}
