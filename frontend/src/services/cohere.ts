cf05c5e30553a51e309127b5466db6c7eae4d452
German Fernandez <germanfernandez0306@gmail.com>
2023-01-27 18:48:55 -0300

    feat: add cohere examples

--- frontend/src/services/cohere.ts
+++ frontend/src/services/cohere.ts
@@ -1,23 +1,31 @@
+import type { CohereResponse } from '../types/cohere'
+import type { Example } from '../types/example'
+
 const ENDPOINT = 'https://api.cohere.ai/classify'
 const COHERE_API_KEY = import.meta.env.PUBLIC_COHERE_API_KEY
 
-type Feeling = 'Feliz' | 'Triste' | 'Aburrido' | 'Cansado' | 'Enojado'
-
-type Example = { text: string; label: Feeling }
-
 const examples: Example[] = [
-	{ text: 'Me siento feliz', label: 'Feliz' },
-	{ text: 'He tenido un dia aburrido', label: 'Aburrido' },
-	{ text: 'Mucho trabajo tuve hoy', label: 'Enojado' },
-	{ text: 'Estoy muy triste', label: 'Triste' },
-	{ text: 'Me siento muy cansado', label: 'Enojado' },
-	{ text: 'Me siento muy feliz', label: 'Feliz' },
-	{ text: 'Me siento muy triste', label: 'Triste' },
-	{ text: 'Me siento muy aburrido', label: 'Aburrido' },
-	{ text: 'Llorando', label: 'Triste' }
+	{ text: 'I feel happy', label: 'Happy' },
+	{ text: 'I pass my exam', label: 'Happy' },
+	{ text: 'I feel sad', label: 'Sad' },
+	{ text: 'I was crying all day', label: 'Sad' },
+	{ text: 'I feel bad thoughts', label: 'Scared' },
+	{ text: 'I am afraid of something', label: 'Scared' },
+	{ text: 'I feel like I want to hit someone', label: 'Annoyed' },
+	{ text: 'I am furious', label: 'Annoyed' },
+	{ text: 'I need to do something with my life', label: 'Bored' },
+	{ text: 'I dont know what to do', label: 'Bored' },
+	{ text: 'I have too much things to do', label: 'Tired' },
+	{ text: 'I did a lot of things today', label: 'Tired' },
+	{ text: 'I am relax', label: 'Chill' },
+	{ text: 'I am calm', label: 'Chill' },
+	{ text: 'I am in love', label: 'Enamored' },
+	{ text: 'I feel enamored with someone', label: 'Enamored' },
+	{ text: 'I felt nervous in the interview', label: 'Nervous' },
+	{ text: 'I feel nervous to speak in public', label: 'Nervous' }
 ]
 
-export const classifyFeelings = async (text) => {
+export const classifyFeelings = async (text): Promise<CohereResponse> => {
 	const request = await fetch(ENDPOINT, {
 		method: 'POST',
 		headers: {
