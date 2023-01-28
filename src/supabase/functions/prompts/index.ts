13400067d4945008a4c531524b3841ad6d4b7812
felipetodev <fe.ossandon.u@gmail.com>
2023-01-28 00:30:28 -0300

    use env variables

--- src/supabase/functions/prompts/index.ts
+++ src/supabase/functions/prompts/index.ts
@@ -1,13 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 import { Example } from '../../../types/example'
 
-const supabaseUrl = 'https://aenwqzueyzbzpootqhfp.supabase.co'
-const supabaseAnonKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlbndxenVleXpienBvb3RxaGZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ3ODEwMTEsImV4cCI6MTk5MDM1NzAxMX0.SnEGaCv0hFvbZ5Z-g6ZpueQAkyhqRLy9uKRgS_dFCHQ'
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-export async function getPromptsData(): Promise<Array<Example>> {
+export async function getPromptsData(): Promise<Example[]> {
 	const { data, error, status } = await supabase.from('prompts').select()
 	return data.map((prompt) => {
 		return {
