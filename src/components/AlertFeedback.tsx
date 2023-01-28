8c89a7b6f3f7aca083f29bb47c579f288467605f
German Fernandez <germanfernandez0306@gmail.com>
2023-01-28 15:26:51 -0300

    feat: add decline option

--- src/components/AlertFeedback.tsx
+++ src/components/AlertFeedback.tsx
@@ -1,24 +1,55 @@
-import { Button } from '../components';
-import { useFeelingStore } from '../hooks';
-import { successPrompt } from '../supabase/functions/prompts';
+import { useState } from 'react'
+import { Button } from '../components'
+import { useFeelingStore } from '../hooks'
+import { labels, successPrompt } from '../supabase/functions/prompts'
+
+const PROMPT_TYPE_STATUS = {
+	SUCCESS: 'SUCCESS',
+	FAILED: 'FAILED'
+}
 
 export const AlertFeedback = () => {
+	const [$feeling] = useFeelingStore()
+	const [showSelect, setShowSelect] = useState<boolean>(false)
+	const [selectedOption, setSelectedOption] = useState<string>('')
 
-    const [$feeling] = useFeelingStore()
-    const handleSuccessProm = async () => {
-        try {
-            await successPrompt({ ...$feeling })
-            // alert('Este boton no hace nada aun')
-            alert('Exitoso')
-        } catch (error) {
-            alert('Fallo supabase feedback')
-        }
-    }
+	const handleSuccessPrompt = async (status: 'SUCCESS' | 'FAILED') => {
+		const data =
+			status === PROMPT_TYPE_STATUS.SUCCESS
+				? { ...$feeling }
+				: { label: selectedOption, text: $feeling.text }
+		try {
+			await successPrompt(data)
+			// alert('Este boton no hace nada aun')
+			alert('Exitoso')
+		} catch (error) {
+			alert('Fallo supabase feedback')
+		}
+	}
 
-    return (
-        <div className='absolute top-0 bg-black p-5 rounded-md flex flex-col gap-5'>
-            <p>was this result useful to you?</p>
-            <Button label='Yes' onClick={handleSuccessProm} className='w-full' />
-        </div>
-    )
+	const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setSelectedOption(e.target.value)
+	}
+
+	return (
+		<div className='absolute top-0 bg-black p-5 rounded-md flex flex-col gap-5'>
+			<p>was this result useful to you?</p>
+			{!showSelect && (
+				<Button label='Yes' onClick={() => handleSuccessPrompt('SUCCESS')} className='w-full' />
+			)}
+			{!showSelect && <Button label='No' onClick={() => setShowSelect(true)} className='w-full' />}
+			{showSelect && (
+				<Button label='Enviar' onClick={() => handleSuccessPrompt('FAILED')} className='w-full' />
+			)}
+			{showSelect && (
+				<select id='feedback-cohere' name='labelOptions' onChange={handleSelect}>
+					{labels
+						.filter((label) => label.toLowerCase() !== $feeling.label)
+						.map((label) => (
+							<option value={label}>{label}</option>
+						))}
+				</select>
+			)}
+		</div>
+	)
 }
