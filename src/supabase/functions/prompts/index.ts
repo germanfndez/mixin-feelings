e14fe1347b3d8c78a74d5b3df97265030f412988
German Fernandez <germanfernandez0306@gmail.com>
2023-01-28 15:25:57 -0300

    feat: add labels
    Co-authored-by: Felipe Ossandon <fe.ossandon.u@gmail.com>
    Co-authored-by: Franklin Martinez  <Franklin361@users.noreply.github.com>

--- src/supabase/functions/prompts/index.ts
+++ src/supabase/functions/prompts/index.ts
@@ -6,14 +6,21 @@ const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
+export let labels = []
+
 export async function getPromptsData(): Promise<Example[]> {
 	const { data, error, status } = await supabase.from('prompts').select()
-	return data.map((prompt) => {
+
+	const prompts = data.map((prompt) => {
 		return {
 			text: prompt.text,
 			label: prompt.label
 		}
 	})
+
+	labels = [...new Set(prompts.map((prompt) => prompt.label))]
+
+	return prompts
 }
 
 export async function successPrompt({ text, label }) {
