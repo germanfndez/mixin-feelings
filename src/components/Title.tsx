2b236dec8f197718aad4889c845ec06ec8d50dd7
German Fernandez <germanfernandez0306@gmail.com>
2023-01-28 16:42:21 -0300

    feat: add spotify and fonts colors

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 export const Title = () => {
 	return (
 		<header className='w-full px-5 my-4 flex justify-start gap-5 select-none items-center'>
-			<h1 className='text-6xl font-[900] text-color-custom'>Mixin Feelings</h1>
+			<h1 className='text-6xl font-bold text-color-custom'>Mixin Feelings</h1>
 			<div className='h-[2px] border-t border-slate-500 flex-1' />
 		</header>
 	)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				mixin: {
+					100: '#1DB954',
+					200: '#1ED760',
+					300: '#FFFFFF',
+					400: '#191414'
+				}
+			},
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 }
