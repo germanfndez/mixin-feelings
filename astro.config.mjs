4863d2f7567bf40ab1be183bb885c8059670619d
felipetodev <fe.ossandon.u@gmail.com>
2023-01-28 00:29:47 -0300

    remove comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from 'astro/config'
-
-// https://astro.build/config
 import svelte from '@astrojs/svelte'
-
-// https://astro.build/config
 import react from '@astrojs/react'
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), react(), tailwind()]
+	integrations: [
+		svelte(),
+		react(),
+		tailwind()]
 })
