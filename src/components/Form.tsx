d753cb8e0ad2398406bd56cd16e94500d5bee54c
felipetodev <fe.ossandon.u@gmail.com>
2023-01-29 20:21:25 -0300

    fix styles blob

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -20,14 +20,14 @@ export function Form() {
 				<div className='relative h-full'>
 					<textarea
 						id='feeling'
-						className='focus:animate-visual absolute z-10 left-0 top-0 bottom-0 right-0 text-mixin-300 resize-none w-full h-full p-5 rounded-md font-semibold bg-mixin-500 outline-none'
+						className='focus:animate-visual absolute z-20 left-0 top-0 bottom-0 right-0 text-mixin-400 resize-none w-full h-full p-5 rounded-md font-semibold outline-none'
 						name='inputFeeling'
 						placeholder='...'
 						disabled={loading}
 					/>
-					<div className="absolute -top-40 right-0 w-[500px] h-[500px] filter blur-xl opacity-40 bg-mixin-100 rounded-full mix-blend-multiply animate-blob animation-delay-4000" />
-    			<div className="absolute -top-20 left-0 w-[500px] h-[500px] filter blur-xl opacity-40 bg-mixin-200 rounded-full mix-blend-multiply animate-blob animation-delay-2000" />
-					<div className='absolute rounded-md h-44 w-full bg-textarea bg-[length:10px_10px] text-mixin-200 opacity-40 -left-6 top-5' />
+					<div className="absolute -top-40 -right-5 w-[500px] h-[500px] filter blur-xl opacity-60 bg-mixin-100 rounded-full mix-blend-multiply animate-blob animation-delay-4000" />
+    			<div className="absolute -top-20 -left-5 w-[500px] h-[500px] filter blur-xl opacity-60 bg-mixin-200 rounded-full mix-blend-multiply animate-blob animation-delay-2000" />
+					<div className='absolute z-10 rounded-md h-44 w-full bg-textarea bg-[length:10px_10px] text-mixin-200 opacity-40 -left-6 top-5' />
 				</div>
 			</div>
 
