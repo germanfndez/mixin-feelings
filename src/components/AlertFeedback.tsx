033b258c139c5b0fa0e6d909223e33149be36f42
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-02-03 23:51:11 -0600

    [feat]: send feadback to supabase

--- src/components/AlertFeedback.tsx
+++ src/components/AlertFeedback.tsx
@@ -23,86 +23,118 @@ export const AlertFeedback = () => {
 
 	const [selectedOption, setSelectedOption] = useState(promptData.label)
 
-	// const handleSentFeedBack = async (status: 'SUCCESS' | 'FAILED') => {
+	const onSelectTypeFeedback = async (type: 'success' | 'wrong') => {
+		if (type === 'success') {
+			await successPrompt(promptData)
+			setAlreadyFeedbackSent(true)
+			customToast({ label: 'Thanks for your feedback!', type: 'success' })
+			return
+		}
 
-
-	// 	// const data =
-	// 	// 	status === PROMPT_TYPE_STATUS.SUCCESS
-	// 	// 		? { ...promptData }
-	// 	// 		: { label: selectedOption, text: promptData.text }
-	// 	// try {
-	// 	// 	await successPrompt(data)
-	// 	// 	customToast({ label: 'Thanks for your feedback!', type: 'success' })
-	// 	// } catch (error) {
-	// 	// 	customToast({
-	// 	// 		label: 'We were unable to process your feedback, please try again later.',
-	// 	// 		type: 'error'
-	// 	// 	})
-	// 	// }
-	// }
-
-
-	const onSendPositiveFeedback = async () => {
-		console.log(promptData)
-		setAlreadyFeedbackSent(true)
+		setShowSelect(true)
 	}
 
-	const onSendNegativeFeedback = () => setShowSelect(true)
-
-	const handleBackFeedback = () => setShowSelect(false)
-
-	const handleSentFeedback = () => {
+	const handleSentFeedback = async () => {
 		const label = selectedOption
 		const text = promptData.text
 
-
+		await successPrompt({ label, text })
 
 		setShowSelect(false)
 		setAlreadyFeedbackSent(true)
+		customToast({ label: 'Thanks for your feedback!', type: 'success' })
 	}
+
+	const handleGoBack = () => setShowSelect(false)
 
 	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => setSelectedOption(e.target.value)
 
 	if (alreadyFeedbackSent) return null
 
 	return (
-		<section className='flex justify-end items-center lg:border-none border-t'>
-			<article className={`flex items-center ${showSelect ? 'gap-3' : 'gap-1'} select-none lg:bg-black/20 bg-transparent lg:rounded-full rounded-none py-1 px-3 text-sm`}>
+		<section className='flex justify-end items-center lg:p-0 pt-4'>
+			<article className={`flex items-center ${showSelect ? 'gap-3 w-full justify-center' : 'gap-1 sm:w-auto w-full sm:justify-end justify-center'} select-none bg-black/20 sm:rounded-full rounded-md sm:py-1 sm:px-3 p-2 text-sm`}>
 
 				{
 					showSelect
-						? <>
-							<label htmlFor="feedback-cohere">Select the option you think is correct</label>
-							<select onChange={handleChange} id='feedback-cohere' name='labelOption' className='bg-mixin-500 text-mixin-300 rounded-md p-1'>
-								{
-									labels.map((label) => (
-										<option className='' key={label} value={label}>{label}</option>
-									))
-								}
-							</select>
-							<Button label='Send' className='!text-sm' withSpinner={false} onClick={handleSentFeedback} />
-							<span >or</span>
-							<Button label='Cancel' className='!text-sm' withSpinner={false} onClick={handleBackFeedback} />
-						</>
-						: <>
-							<span>Was this result useful to you:</span>
-							<Button
-								withSpinner={false}
-								className='hover:!bg-red-500 active:!bg-red-600 !bg-transparent !rounded-full w-7 h-7 grid place-items-center'
-								onClick={onSendPositiveFeedback}
-							>
-								<HearthIcon />
-							</Button>
-							<Button
-								withSpinner={false}
-								className='hover:!bg-black active:!bg-black/70 !bg-transparent !rounded-full w-7 h-7 grid place-items-center'
-								onClick={onSendNegativeFeedback}
-							>
-								<CloseIcon />
-							</Button>
-						</>
+						? <SelectLabel
+							handleGoBack={handleGoBack}
+							onChange={handleChange}
+							handleSentFeedback={handleSentFeedback}
+							currentLabel={promptData.label}
+						/>
+						: <AlertFeedbackButtons
+							handleWrongFeedback={() => onSelectTypeFeedback('wrong')}
+							handleCorrectFeedback={() => onSelectTypeFeedback('success')}
+						/>
 				}
 			</article>
 		</section>
+	)
+}
+
+interface PropsAlertFeedbackButtons {
+	handleCorrectFeedback: () => void
+	handleWrongFeedback: () => void
+}
+
+export const AlertFeedbackButtons = ({
+	handleWrongFeedback,
+	handleCorrectFeedback
+}: PropsAlertFeedbackButtons) => {
+	return (
+		<>
+			<span>Was this result useful to you:</span>
+			<Button
+				withSpinner={false}
+				className='hover:!bg-red-500 active:!bg-red-600 !bg-transparent !rounded-full w-7 h-7 grid place-items-center'
+				onClick={handleCorrectFeedback}
+			>
+				<HearthIcon />
+			</Button>
+			<Button
+				withSpinner={false}
+				className='hover:!bg-black active:!bg-black/70 !bg-transparent !rounded-full w-7 h-7 grid place-items-center'
+				onClick={handleWrongFeedback}
+			>
+				<CloseIcon />
+			</Button>
+		</>
+	)
+}
+
+
+interface PropsSelectLabel {
+	onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
+	handleGoBack: () => void
+	handleSentFeedback: () => void
+	currentLabel: string
+}
+
+export const SelectLabel = ({
+	onChange,
+	handleGoBack,
+	handleSentFeedback,
+	currentLabel
+}: PropsSelectLabel) => {
+
+	return (
+		<div className='flex sm:flex-row flex-col sm:gap-4 gap-2 items-center justify-center w-full'>
+			<label htmlFor="feedback-cohere">Select the option you think is correct</label>
+			<select onChange={onChange} id='feedback-cohere' name='labelOption' className='bg-mixin-500 text-mixin-300 rounded-md p-1  border sm:w-auto w-full'>
+				{
+					labels
+						.filter(label => label !== currentLabel)
+						.map((label) => (
+							<option className='' key={label} value={label}>{label}</option>
+						))
+				}
+			</select>
+			<div className='flex gap-2 items-center justify-center'>
+				<Button label='Send' className='!text-sm h-7' withSpinner={false} onClick={handleSentFeedback} />
+				<span >or</span>
+				<Button label='<-' className='!text-sm h-7' withSpinner={false} onClick={handleGoBack} />
+			</div>
+		</div>
 	)
 }
--- src/components/FeelingResult.tsx
+++ src/components/FeelingResult.tsx
@@ -6,12 +6,12 @@ interface Props {
 }
 
 export const FeelingResult = ({ feelingClassified, existsPlaylist }: Props) => {
-	// if (!feelingClassified) return null
+	if (!feelingClassified) return null
 
 	return (
 		<div className='relative z-10 inline-block bg-[length:400%_400%] p-2 rounded-md text-2xl bg-gradient-to-r from-mixin-100 to-[#c5fa70] via-[#e0f0c7] '>
 			{
-				!existsPlaylist ? (
+				existsPlaylist ? (
 					<div className='flex flex-col gap-4 py-5 px-7'>
 						<span>
 							So if you are feeling{' '}
