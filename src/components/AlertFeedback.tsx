26fc499fe77d56abfbdaceed74b96039a8f0d89e
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-02-06 11:39:48 -0600

    style: change design of AlertFeedback component

--- src/components/AlertFeedback.tsx
+++ src/components/AlertFeedback.tsx
@@ -4,17 +4,12 @@ import { successPrompt } from '../supabase/functions/prompts'
 import { customToast, getLabels } from '../utils'
 import { Button } from './shared/Button'
 import CloseIcon from './shared/CloseIcon'
-import HearthIcon from './shared/HearthIcon'
-
-const PROMPT_TYPE_STATUS = {
-	SUCCESS: 'SUCCESS',
-	FAILED: 'FAILED'
-}
+import LikeIcon from './shared/LikeIcon'
+import DislikeIcon from './shared/DislikeIcon'
 
 const labels = getLabels()
 
 export const AlertFeedback = () => {
-
 	const [alreadyFeedbackSent, setAlreadyFeedbackSent] = useState(false)
 
 	const [promptData] = usePrompDataStore()
@@ -47,27 +42,33 @@ export const AlertFeedback = () => {
 
 	const handleGoBack = () => setShowSelect(false)
 
-	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => setSelectedOption(e.target.value)
+	const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) =>
+		setSelectedOption(e.target.value)
 
 	if (alreadyFeedbackSent) return null
 
 	return (
 		<section className='flex justify-end items-center lg:p-0 pt-4'>
-			<article className={`flex items-center ${showSelect ? 'gap-3 w-full justify-center' : 'gap-1 sm:w-auto w-full sm:justify-end justify-center'} select-none bg-black/20 sm:rounded-full rounded-md sm:py-1 sm:px-3 p-2 text-sm`}>
-
-				{
+			<article
+				className={`flex items-center ${
 					showSelect
-						? <SelectLabel
-							handleGoBack={handleGoBack}
-							onChange={handleChange}
-							handleSentFeedback={handleSentFeedback}
-							currentLabel={promptData.label}
-						/>
-						: <AlertFeedbackButtons
-							handleWrongFeedback={() => onSelectTypeFeedback('wrong')}
-							handleCorrectFeedback={() => onSelectTypeFeedback('success')}
-						/>
-				}
+						? 'gap-3 w-full justify-center'
+						: 'gap-1 sm:w-auto w-full sm:justify-end justify-center'
+				} select-none bg-black/20 sm:rounded-full rounded-md sm:py-1 sm:px-3 p-2 text-sm`}
+			>
+				{showSelect ? (
+					<SelectLabel
+						handleGoBack={handleGoBack}
+						onChange={handleChange}
+						handleSentFeedback={handleSentFeedback}
+						currentLabel={promptData.label}
+					/>
+				) : (
+					<AlertFeedbackButtons
+						handleWrongFeedback={() => onSelectTypeFeedback('wrong')}
+						handleCorrectFeedback={() => onSelectTypeFeedback('success')}
+					/>
+				)}
 			</article>
 		</section>
 	)
@@ -90,19 +91,18 @@ export const AlertFeedbackButtons = ({
 				className='hover:!bg-red-500 active:!bg-red-600 !bg-transparent !rounded-full w-7 h-7 grid place-items-center'
 				onClick={handleCorrectFeedback}
 			>
-				<HearthIcon />
+				<LikeIcon />
 			</Button>
 			<Button
 				withSpinner={false}
 				className='hover:!bg-black active:!bg-black/70 !bg-transparent !rounded-full w-7 h-7 grid place-items-center'
 				onClick={handleWrongFeedback}
 			>
-				<CloseIcon />
+				<DislikeIcon />
 			</Button>
 		</>
 	)
 }
-
 
 interface PropsSelectLabel {
 	onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
@@ -117,23 +117,37 @@ export const SelectLabel = ({
 	handleSentFeedback,
 	currentLabel
 }: PropsSelectLabel) => {
-
 	return (
 		<div className='flex sm:flex-row flex-col sm:gap-4 gap-2 items-center justify-center w-full'>
-			<label htmlFor="feedback-cohere">Select the option you think is correct</label>
-			<select onChange={onChange} id='feedback-cohere' name='labelOption' className='bg-mixin-500 text-mixin-300 rounded-md p-1  border sm:w-auto w-full'>
-				{
-					labels
-						.filter(label => label !== currentLabel)
-						.map((label) => (
-							<option className='' key={label} value={label}>{label}</option>
-						))
-				}
+			<label htmlFor='feedback-cohere'>Select the option you think is correct</label>
+			<select
+				onChange={onChange}
+				id='feedback-cohere'
+				name='labelOption'
+				className=' text-mixin-400 rounded-md h-7  border sm:w-auto w-full'
+			>
+				{labels
+					.filter((label) => label !== currentLabel)
+					.map((label) => (
+						<option className='' key={label} value={label}>
+							{label}
+						</option>
+					))}
 			</select>
 			<div className='flex gap-2 items-center justify-center'>
-				<Button label='Send' className='!text-sm h-7' withSpinner={false} onClick={handleSentFeedback} />
-				<span >or</span>
-				<Button label='<-' className='!text-sm h-7' withSpinner={false} onClick={handleGoBack} />
+				<Button
+					label='Send'
+					className='!text-sm h-7 !bg-mixin-200 !text-black hover:!bg-mixin-100 border border-mixin-hover'
+					withSpinner={false}
+					onClick={handleSentFeedback}
+				/>
+				<span>|</span>
+				<Button
+					label='Cancel'
+					className='!text-sm h-7 !bg-transparent text-red-700 hover:!bg-red-500 hover:text-white'
+					withSpinner={false}
+					onClick={handleGoBack}
+				/>
 			</div>
 		</div>
 	)
