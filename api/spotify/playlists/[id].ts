20952cc2bccc2ef0ed5e3b3433682b4d55e5cce8
felipetodev <fe.ossandon.u@gmail.com>
2023-02-06 15:10:33 -0300

    feat: add api routes correctly

--- api/spotify/playlists/[id].ts
+++ api/spotify/playlists/[id].ts
@@ -3,24 +3,23 @@ export const config = {
   regions: ['iad1']
 }
 
-const { PUBLIC_RAPIDAPI_KEY = '', PUBLIC_RAPIDAPI_HOST = '' } = process.env
-
-export default async (req: any) => {
-  const { id = '' } = req && req.query || {}
+export default async (req: Request, res: any) => {
+	const { 1: id } = req.url.split("?id=")
+	try {
+		const resp = await fetch(
+			`https://spotify23.p.rapidapi.com/playlist/?id=${id}`,
+			{
+				method: 'GET',
+				headers: {
+					'X-RapidAPI-Key': process.env.PUBLIC_RAPIDAPI_KEY,
+					'X-RapidAPI-Host': process.env.PUBLIC_RAPIDAPI_HOST
+				}
+			}
+		)
+		const res = (await resp.json())
 	
-		// const resp = await fetch(
-		// 	`https://spotify23.p.rapidapi.com/playlist_tracks/?id=${id}&offset=0&limit=1`,
-		// 	{
-		// 		method: 'GET',
-		// 		headers: {
-		// 			'X-RapidAPI-Key': `${PUBLIC_RAPIDAPI_KEY}`,
-		// 			'X-RapidAPI-Host': `${PUBLIC_RAPIDAPI_HOST}`
-		// 		}
-		// 	}
-		// )
-		// const { items } = (await resp.json()) as { items: Array<{ track: { preview_url: string }}> }
-		// const [{ track }] = items
-	
-		// return res.json(track)
-	return new Response(`Error: ${id} - ${PUBLIC_RAPIDAPI_KEY} - ${PUBLIC_RAPIDAPI_HOST}`);
+		return new Response(JSON.stringify(res))
+	} catch (error) {
+		return new Response(JSON.stringify({ message: "Something went wrong", error }))
+	}
 }
